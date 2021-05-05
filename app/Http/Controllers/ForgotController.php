bb09bb6b60e8c4855a65e6ebb2b2a70c973b3485
yassine dev <lagyassine88@gmail.com>
2021-05-05 17:57:55 +0000

    Envoi d'un mail pour réinitialiser le mot de passe

--- app/Http/Controllers/ForgotController.php
+++ app/Http/Controllers/ForgotController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\PasswordResetNotification;
+use App\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ForgotController extends Controller
 {
@@ -19,6 +24,21 @@ class ForgotController extends Controller
         $request->validate([
             'email' => 'required|email|exists:users',
         ]);
-        return 'Vérification réussi';
+        // return 'Vérification réussi';
+
+        $token = Str::uuid();
+
+        DB::table('password_resets')->insert([
+            'email'=>request('email'),
+            'token'=>$token,
+            'created_at'=>now(),
+        ]);
+
+        $user = User::whereEmail(request('email'))->firstOrFail();
+        $user->notify(new PasswordResetNotification($token));
+
+        $success = 'Vérifier votre boite mail et suivez les instructions.';
+        return back()->withSuccess($success);
+
     }
 }
---
+++ app/Notifications/PasswordResetNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordResetNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(string $token)
+    {
+        //
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->greeting('Bonjour '.$notifiable->name)
+                    ->subject('Oublie de mot de passe')
+                    ->line('Veuillez cliquer sur le lien ci dessous pour rénitialiser le mot de passe.')
+                    ->action('Rénitialiser ', url('reset/'.$this->token))
+                    ->salutation('Merci d\'utiliser notre application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
